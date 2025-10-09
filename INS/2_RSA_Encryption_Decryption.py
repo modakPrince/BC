295fff05412088cc783020d8845604a0b50fa665
modakprince@proton.me <modakprince@proton.me>
2025-10-09 10:36:10 +0530

    INS

---
+++ INS/2_RSA_Encryption_Decryption.py
@@ -0,0 +1,21 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import binascii
+
+keyPair =RSA.generate(1024)
+
+pubKey = keyPair.publickey() 
+print(f"Public key:(n={hex(pubKey.n)}, e={hex(pubKey.e)})") 
+
+pubKeyPEM =pubKey.exportKey() 
+print(pubKeyPEM.decode('ascii'))
+print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+
+privKeyPEM = keyPair.exportKey()
+print(privKeyPEM.decode('ascii'))
+
+#encryption msg = 'Ismile Academy'
+msg = input("Enter the message to encrypt: ")
+encryptor = PKCS1_OAEP.new(pubKey)
+encrypted = encryptor.encrypt(msg.encode('utf-8'))
+print("Encrypted:", binascii.hexlify(encrypted))
